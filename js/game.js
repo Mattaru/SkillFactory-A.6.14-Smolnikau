1ccb091654be1c7792cca5134ea66eb9c1999985
Mattaru <67161027+Mattaru@users.noreply.github.com>
2020-06-27 17:57:28 +0300

    A6.14-Created all general functions and added all needed HTML elements

--- js/game.js
+++ js/game.js
@@ -4,36 +4,37 @@ const maxHits = 10;
 let hits = 0;
 let firstHitTime = 0;
 
-function round() {
-  // FIXME: надо бы убрать "target" прежде чем искать новый
-
+function round() { 
   let divSelector = randomDivId();
   $(divSelector).addClass("target");
-  // TODO: помечать target текущим номером
+  // Помечание таргета текущим номером
   $(divSelector).html(`${divSelector.slice(-2)}`);
-  // FIXME: тут надо определять при первом клике firstHitTime
-  
+  // После первого клика на .таргет - переменная записывает значение таймера
+  $(divSelector).one("click", function() {
+    firstHitTime = getTimestamp();
+  });
   if (hits === maxHits) {
     endGame();
   }
 }
 
 function endGame() {
-  // FIXME: спрятать игровое поле сначала
-
+  // Прячем игровое поле, оставляя результат игры и кнопку "Играть заново"
+  $(".score").removeClass("visible");
   let totalPlayedMillis = getTimestamp() - firstHitTime;
   let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
   $("#total-time-played").text(totalPlayedSeconds);
-
   $("#win-message").removeClass("d-none");
 }
 
+// Событие клика по ячейке
 function handleClick(event) {
-
   if ($(event.target).hasClass("target")) {
     hits = hits + 1;
     // Убераем текст с ячеек таргетов
     $(".target").html("");
+    // Убераем .таргет с ячеек
+    $(event.target).removeClass("target");
     round();
   } else {
     // Отмечаем промахи красным фоном на 0.5с.
@@ -46,9 +47,7 @@ function handleClick(event) {
 
 // Механизм запуска
 function init() {
-
   round();
-
   $(".game-field").click(handleClick);
   $("#button-reload").click(function() {
     location.reload();
@@ -59,6 +58,7 @@ function init() {
 $(".start-btn").click(function() {
   $(".start-btn").removeClass("visible");
   $(".game-board").addClass("visible");
+  $(".score").addClass("visible");
   init();
 });
 
